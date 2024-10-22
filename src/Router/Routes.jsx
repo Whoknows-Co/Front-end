6f5062ea7c9faa3a4c87853f30de85b2de32ee90
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-10-22 22:07:07 +0330

    install react router package and config router file

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
