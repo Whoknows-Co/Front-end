d0c9199dc8076de44cc7323c9a242ea0eeabe42d
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-10-24 12:29:59 +0330

    header created

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import styles from "./Header.module.css";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <img src="../../public/logo.svg" alt="" />
+      <div className={styles.navbar}>
+        <div>
+          <p>نوبت دهی مشاوره</p>
+          <img src="../../public/icons8-dropdown-30 3.svg" alt="" />
+        </div>
+        <div>
+          <p>مشاوره درسی آنلاین</p>
+          <img src="../../public/icons8-dropdown-30 3.svg" alt="" />
+        </div>
+        <div>
+          <p>آموزشگاها</p>
+          <img src="../../public/icons8-dropdown-30 3.svg" alt="" />
+        </div>
+        <div>
+          <p>مشاوران</p>
+          <img src="../../public/icons8-dropdown-30 3.svg" alt="" />
+        </div>
+      </div>
+      <button>ورود/ثبت نام</button>
+    </div>
+  );
 }
 
 export default Header;
