249492fd56f73eed6d93dabb43b124b24c9569db
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-01 23:46:47 +0330

    consult  info card content updated

--- src/components/module/ConsultInfoCard.jsx
+++ src/components/module/ConsultInfoCard.jsx
@@ -4,7 +4,31 @@ function ConsultInfoCard() {
   return (
     <div className={styles.container}>
       <div className={styles.background}></div>
-      <h2 className={styles.any}>content</h2>
+      <img
+        className={styles.bullets}
+        src="../../../public/icons/tirpleBullet.svg"
+        alt=""
+      />
+      <div className={styles.header}>
+        <img src="../../../public/icons/profile-icon.svg" alt="" />
+        <div className={styles.details}>
+          <h2>پرنیان خالقی</h2>
+          <h3>مشاور رشته ریاضی</h3>
+          <div>
+            <img src="../../../public/icons/Location.svg" alt="" />
+            <p>شیراز</p>
+          </div>
+          <div>
+            <img src="../../../public/icons/trust.svg" alt="" />
+            <p>مشاور موسسه سوتک</p>
+          </div>
+          <div>
+            <img src="../../../public/icons/email.svg" alt="" />
+            <p>ParnianKhaleghi94@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}></div>
     </div>
   );
 }
