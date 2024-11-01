62521487dac93631c9960b1d96d9b3fbe38f0d65
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-01 22:38:25 +0330

    footer created

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import styles from "./Footer.module.css";
+import cities from "../constants/cities";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <h3>شهر های منتورو</h3>
+        {cities.map((city) => (
+          <p>{city.name}</p>
+        ))}
+      </div>
+      <div>
+        <h3>درباره منتورو</h3>
+        <p>مشاوره تحصیلی آنلاین</p>
+        <p>درباره‌ی ما</p>
+        <p>سوالات متداول</p>
+        <p>ثبت‌نام مشاوران</p>
+        <p>قوانین و مقررات</p>
+        <p>قوانین ثبت نظر</p>
+        <p>تماس با ما</p>
+      </div>
+      <div className={styles.contactUs}>
+        <h3>با ما در ارتباط باشید</h3>
+        <p>mentoro.ac@gmail.com</p>
+        <img src="../../public/logo.svg" alt="" />
+      </div>
+      <div>
+        <h3>منتورو را در شبکه های اجتماعی دنبال کنید</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
---
+++ src/constants/cities.js
@@ -0,0 +1,8 @@
+const cities = [
+  { name: "شیراز" },
+  { name: "اصفهان" },
+  { name: "یزد" },
+  { name: "تهران" },
+  { name: "یاسوج" },
+];
+export default cities;
