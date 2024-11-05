4fa25a4c41f5381c33a922015db8f927aee2641b
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-05 21:49:59 +0330

    updating reservation card

--- src/components/module/ReservationCard.jsx
+++ src/components/module/ReservationCard.jsx
@@ -3,7 +3,24 @@ import styles from "./ReservationCard.module.css";
 function ReservationCard() {
   return (
     <div className={styles.container}>
-      <h1>content</h1>
+      <div className={styles.header}>
+        <div>
+          <img src="../../../public/icons/Educatoin.svg" alt="" />
+          <p>نوبت دهی اینترنتی با پرنیان خالقی</p>
+        </div>
+        <span>موسسه ی آموزش های عالی دانشگاه صنعتی شیراز</span>
+      </div>
+      <div className={styles.main}>
+        <div>
+          <img src="../../../public/icons/calender.svg" alt="" />
+          <p>انتخاب نوبت مشاوره:</p>
+        </div>
+        <select name="" id=""></select>
+      </div>
+      <hr className={styles.dashed} />
+      <div className={styles.footer}>
+        <button>نوبت بگیرید</button>
+      </div>
     </div>
   );
 }
