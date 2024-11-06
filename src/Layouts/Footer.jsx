71f9248c2c9f492bdb948349a323b7780b2f11d1
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-06 21:57:32 +0330

    updateing footer

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -2,32 +2,46 @@ import styles from "./Footer.module.css";
 import cities from "../constants/cities";
 function Footer() {
   return (
-    <div className={styles.container}>
-      <div>
-        <h3>شهر های منتورو</h3>
-        {cities.map((city) => (
-          <p>{city.name}</p>
-        ))}
+    <>
+      <div className={styles.container}>
+        <div>
+          <h3>شهر های منتورو</h3>
+          {cities.map((city) => (
+            <p>{city.name}</p>
+          ))}
+        </div>
+        <div>
+          <h3>درباره منتورو</h3>
+          <p>مشاوره تحصیلی آنلاین</p>
+          <p>درباره‌ی ما</p>
+          <p>سوالات متداول</p>
+          <p>ثبت‌نام مشاوران</p>
+          <p>قوانین و مقررات</p>
+          <p>قوانین ثبت نظر</p>
+          <p>تماس با ما</p>
+        </div>
+        <div className={styles.contactUs}>
+          <h3>با ما در ارتباط باشید</h3>
+          <p>mentoro.ac@gmail.com</p>
+          <img src="../../public/logo.svg" alt="" />
+        </div>
+        <div>
+          <h3>منتورو را در شبکه های اجتماعی دنبال کنید</h3>
+        </div>
       </div>
-      <div>
-        <h3>درباره منتورو</h3>
-        <p>مشاوره تحصیلی آنلاین</p>
-        <p>درباره‌ی ما</p>
-        <p>سوالات متداول</p>
-        <p>ثبت‌نام مشاوران</p>
-        <p>قوانین و مقررات</p>
-        <p>قوانین ثبت نظر</p>
-        <p>تماس با ما</p>
+      <div className={styles.describtion}>
+        <p>
+          <span>منتورو: </span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از
+          صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه
+          روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+          تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+          باشد،
+        </p>
       </div>
-      <div className={styles.contactUs}>
-        <h3>با ما در ارتباط باشید</h3>
-        <p>mentoro.ac@gmail.com</p>
-        <img src="../../public/logo.svg" alt="" />
+      <div className={styles.rule}>
+        <p>کلیه حقوق این سایت متعلق به شرکت WhoKnows می‌باشد.</p>
       </div>
-      <div>
-        <h3>منتورو را در شبکه های اجتماعی دنبال کنید</h3>
-      </div>
-    </div>
+    </>
   );
 }
 
